54a9cfb2b7c2a595f0bb75cd0a3805b0fe021bbd
Colin Wilson <colin.wilson@protonmail.ch>
2020-11-26 15:50:49 +0000

    🔧 bump default Traefik image to 2.3.4 📄 readme update

--- variables.tf
+++ variables.tf
@@ -31,7 +31,7 @@ variable "traefik_network_attachable" {
 variable "traefik_version" {
   type        = string
   description = "Traefik Docker image version."
-  default     = "2.3.2" # https://github.com/traefik/traefik/releases/latest
+  default     = "2.3.4" # https://github.com/traefik/traefik/releases/latest
 }
 
 variable "password" {
