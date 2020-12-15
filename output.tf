3009b142706df3bc954a9b9418bdfd167019c354
Colin Wilson <colin.wilson@protonmail.ch>
2020-12-15 04:10:57 +0000

    ➕ add descriptions to outputs.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,14 @@
 output "acme_volume_mountpoint" {
-  value = docker_volume.traefik_acme.mountpoint
+  description = "Mount point of the ACME certificates storage volume"
+  value       = docker_volume.traefik_acme.mountpoint
 }
 
 output "traefik_network_name" {
-  value = docker_network.network.name
+  description = "Traefik overlay network name"
+  value       = docker_network.network.name
 }
 
 output "traefik_service_config_name" {
-  value = docker_config.traefik-yaml.name
+  description = "Name of generated Traefik configuration file"
+  value       = docker_config.traefik-yaml.name
 }
